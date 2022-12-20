1b2c4562fba6c187f2a1b5151f4486f87146b7e5
germanpagano <sergiogermanpagano@gmail.com>
2022-12-19 21:50:19 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mi E-commerce!
-        </p>
-      </header>
+
+      Aca va el carrito 
     </div>
   );
 }
